790dea98a589363b0016ad24f90f7d661f6ceb55
bhavenvi <gorebhaven7@gmail.com>
2023-12-30 11:33:16 -0800

    Initial Commit

---
+++ TrojanFoods/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrojanFoods
+//
+//  Created by Bhaven Gore on 12/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrojanFoods/TrojanFoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrojanFoodsApp.swift
+//  TrojanFoods
+//
+//  Created by Bhaven Gore on 12/30/23.
+//
+
+import SwiftUI
+
+@main
+struct TrojanFoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
